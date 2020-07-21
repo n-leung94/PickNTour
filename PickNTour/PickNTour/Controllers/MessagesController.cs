06dd5d5efb02259433adaba12b117e2de83babcb
n-leung94 <56689279+n-leung94@users.noreply.github.com>
2020-07-21 15:22:52 +0800

    Send Personal Message Function
    Description: Basic Implementation of Sending a personal message to another user of the site using a Message Viewmodel that will be created from the Compose Form and sent to the controller for processing to insert an an actual Message model into the Db. The future plans for this is to fully convert this function into an AJAX based web form instead of a ViewModel and also to implement Typeahead feature to query all the possible users in the DB and allow the user to select a valid user before sending the message.
    
    Added:
      - MessagesController
          - Compose()
          - Send() [HTTPPost]
    
    ViewModel:
      - SendMessageFormViewModel
    
    Views --> Messages:
      - Compose

---
+++ PickNTour/PickNTour/Controllers/MessagesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using PickNTour.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using PickNTour.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using PickNTour.Models;
+using PickNTour.Data;
+
+namespace PickNTour.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private ApplicationDbContext _context { get; set; }
+        private UserManager<ApplicationUser> _userManager { get; set; }
+
+        public MessagesController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+
+        public IActionResult Compose()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Send(SendMessageFormViewModel viewModel)
+        {
+            var recipientInDb = _context.Users.SingleOrDefault(u => u.UserName == viewModel.UserName);
+
+            if (recipientInDb == null)
+                return NotFound("The Username you are attempting to send a message to, does not exist");
+
+            var currUser = _userManager.GetUserId(HttpContext.User);
+
+            var newMessage = new Message
+            {
+                UserToId = recipientInDb.Id,
+                UserFromId = currUser,
+                DateSent = DateTime.Now,
+                Subject = viewModel.Subject,
+                MessageBody = viewModel.MessageBody
+            };
+
+            _context.Add(newMessage);
+            _context.SaveChanges();
+
+            return RedirectToAction("Compose", "Messages");
+        }
+    }
+}
---
+++ PickNTour/PickNTour/ViewModels/SendMessageFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PickNTour.ViewModels
+{
+    public class SendMessageFormViewModel
+    {
+        [Required]
+        [Display(Name = "Recipient Username")]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string MessageBody { get; set; }
+    }
+}
