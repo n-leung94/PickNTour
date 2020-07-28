f984453df19ab8afb47500bbc6679bd23e9dac4f
n-leung94 <56689279+n-leung94@users.noreply.github.com>
2020-07-28 18:57:41 +0800

    [Message] AutoComplete UserName
    Description: When a message is being composed, typing a minimum of 3 letters in the Recipient Username Input box will now trigger a query to the API end point and query for a list of possible valid users which usernames that contains the letters and return as a selectable list to the sender so that they'll be sending it to a valid user.
    
    Added:
    
    - Controller[API]:
    - UserQueryController[API]
       - QueryUser(query) [HTTPGET]
    
    - Scripts:
     - typeahead.js

---
+++ PickNTour/PickNTour/Controllers/api/UserQueryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PickNTour.Models;
+using PickNTour.Data;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using PickNTour.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+using PickNTour.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace PickNTour.Controllers.api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserQueryController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserQueryController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet("{query}")]
+        public IActionResult QueryUser(string query)
+        {
+
+            var users = _context.Users.Where(u => u.UserName.Contains(query));
+
+            var userDto = users.ToList().Select(_mapper.Map<ApplicationUser, UserQueryDto>);
+
+            return Ok(userDto);
+
+        }
+
+    }
+
+
+}
